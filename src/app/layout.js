3f14084092615050664e41f241f54303d43e03ff
VanTho <ovantho094@gmail.com>
2025-08-22 08:39:08 +0700

    icon

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,12 +6,15 @@ import NavWrapper from "@/components/NavWrapper/NavWrapper";
 import { SearchProvider } from "@/context/SearchContext";
 import { QuantityOrderProvider } from "@/context/QuantityOrderProvider";
 import { LoadingProvider } from "@/context/LoadingContext";
-import GlobalLoadingBar from "@/components/Loading/GlobalLoadingBar";
 
 export const metadata = {
   title: "Gym Store",
   description: "Gym Store and PT Searcher",
-  icon: "/logo.png",
+  icons: {
+    icon: "/favicon.ico", 
+    shortcut: "/favicon.ico",
+    apple: "/logo.png",
+  },
 };
 
 export default function RootLayout({ children }) {
